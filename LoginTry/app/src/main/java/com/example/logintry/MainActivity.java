8dab47f7d9b415908188410aad444064e306e108
Benjo <tolnaibenedek25@gmail.com>
2024-10-03 10:39:17 +0200

    login

---
+++ LoginTry/app/src/main/java/com/example/logintry/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.logintry;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    /*
+    EditText Felhasznev = findViewById(R.id.FelhaszNev);
+    EditText Jelszo = findViewById(R.id.jelszo);
+    Button Bejelentkezes = findViewById(R.id.bejelentkezesButton);
+    */
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public void register(View v){
+        Intent i = new Intent(this, RegisterActivity.class);
+        startActivity(i);
+    }
+}
